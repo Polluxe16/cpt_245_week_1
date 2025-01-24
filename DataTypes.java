4df154501ab466979ccfd238331c679011f54d8d
Polluxe <Polluxe16@outlook.com>
2025-01-24 15:49:53 -0500

    Update DataTypes.java

--- DataTypes.java
+++ DataTypes.java
@@ -1,19 +1,19 @@
 public class DataTypes {
     
 
-    private static void displayValues(String name, int age, double cashOnHand, boolean isStudent) {
+    private static void main(String name, int age, double cashOnHand, boolean isStudent) { // Swapped the main and displayValues methods
 
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
-        System.out.println("Cash On Hand: " + casOnHand);
+        System.out.println("Cash On Hand: " + cashOnHand); // Corrected the typo on cashOnHand
         System.out.println("Is Student: " + isStudent);
     }
 
-    public static void main(String[] args) {
+    public static void displayValues(String[] args) {
 
         String name = "Brett";
         int age = 25;
-        double cashOnHand = 10.25;
+        double cashOnHand = 10.25d; //Added a d to the end of the double value
         boolean isStudent = false;
         
 
