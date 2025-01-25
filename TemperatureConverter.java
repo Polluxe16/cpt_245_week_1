fb9f9aaecd86f6dfdc8a7bf8b5a963a1fa629e32
Polluxe <Polluxe16@outlook.com>
2025-01-25 09:52:33 -0500

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,4 +1,4 @@
-public class TemperatureConverter {
+ublic class TemperatureConverter {
     
 
     private static void convertToCelsius(double degreesInFahrenheit) {
@@ -10,10 +10,11 @@ public class TemperatureConverter {
          * Please write an "algorithm" below taking the "input parameter" degreesInFarenheit
          * and converting it to the variable "degreesInCelsius" which will be printed below
          *  
-         */
+        */
 
-        
-
+        double degreesInCelsius = 5.0/9.0 * (degreesInFahrenheit - 32);
+        // Made sure to make 5/9 decimals, outputted -0.0 when ints
+        // This formlua outputted the correct answer, assuming the answer I got from Google was the right one
         
 
 
@@ -25,7 +26,7 @@ public class TemperatureConverter {
 
     }
 
-    public static void main(System[] args) {
+    public static void main(String[] args) { // Replaced System with String to fix the error
 
         double degreesInFahrenheit = 25;
         
